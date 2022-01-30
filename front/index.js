6929ea97a2d0f6017cf908fbdfcc680f051cdcbc
Carlos Eduardo Pereira <cedudu081@hotmail.com>
2022-01-30 01:30:36 -0300

    first commit

---
+++ front/index.js
@@ -0,0 +1,71 @@
+const letterBoxesMatch = document.getElementsByClassName('letter-box-match');
+const letterBoxesGuess = document.getElementsByClassName('letter-box-guess');
+// const letterBoxesWrong = document.getElementsByClassName('letter-box-wrong');
+const lettersWrong = document.querySelectorAll('input[type=checkbox]')
+const btnSend = document.getElementById('submit');
+const tablePossibilities = document.getElementById('possibilities');
+
+[...letterBoxesMatch].forEach(match => {
+})
+
+let possibilities = [];
+
+btnSend.addEventListener('click', async (e) => {
+    let letters = {
+        match: {},
+        guess: {},
+        wrong: []
+    };
+    Object.values({ ...letterBoxesMatch }).forEach((k) => {
+        letters.match[k.id] = k.value;
+    });
+
+    Object.values({ ...letterBoxesGuess }).forEach((k) => {
+        letters.guess[k.id] = k.value;
+    });
+
+    [ ...lettersWrong].forEach(k => {
+        if(k.checked) {
+            letters.wrong.push(k.value);
+        }
+    })
+    
+    await submit(letters);
+
+    renderTable();
+})
+
+const submit = async (letters) => {
+    const rawResponse = await fetch('http://localhost:3000', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(letters)
+    });
+    possibilities = await rawResponse.json();
+  };
+
+const renderTable = () => {
+    tablePossibilities.textContent = ''
+    let index = 0;
+    
+    for(let i=0;i<possibilities.length;i++) {
+        let tr = document.createElement('tr');
+    
+        for(let j=0;j<8;j++, i++){
+            const possibilitie = possibilities[i];
+            if(possibilitie) {
+                const th = document.createElement('th');
+                const text = document.createTextNode(possibilities[i])
+        
+                th.appendChild(text);
+                tr.appendChild(th);
+            }
+        }
+        tablePossibilities.appendChild(tr);
+    }
+
+    
+}
---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const { readFile } = require('fs');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.post('/',(req, res) => {
+    console.log(req.body);
+    const body = req.body
+    procurarPalavras(req, res, body)
+})
+
+const procurarPalavras = (req, res, body) => {
+    const { match, guess, wrong } = body;
+    readFile('word.txt', (err, data) => {
+        if (err) throw err;
+        let palavras = data.toString('utf-8').toLocaleLowerCase().split(' ');
+    
+        const palavrasFiltradas = palavras.filter(palavra => {
+            const from = "ãàáäâèéëêìíïîòóöôùúüûñ";
+            const to   = "aaaaaeeeeiiiioooouuuun";
+            for (var i=0, l=from.length ; i<l ; i++) {
+                palavra = palavra.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+            }
+    
+            for(let i=0; i<5; i++) {
+                if (match[i]) {
+                    if(!(palavra.split('')[i] === (match[i]))) {
+                        return false;
+                    }
+                }
+            }
+
+            const guesses = Object.values(guess).filter(k => k !== '');
+
+            for(let i=0; i<guesses.length; i++){
+                if(!(palavra.indexOf(guesses[i]) > -1)){
+                    return false;
+                }
+            }
+
+            for(let i=0; i<wrong.length; i++){
+                if(palavra.indexOf(wrong[i]) > -1){
+                    return false;
+                }
+            }
+
+            return palavra;
+            })
+    
+            res.send(palavrasFiltradas);
+    });
+}
+
+app.listen(3000, () => {
+    console.log('rodando na porta 3000')
+});
