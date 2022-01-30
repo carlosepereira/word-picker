b16d1650c23398a834fdb3ce97afd389c86437eb
Carlos Eduardo Pereira <cedudu081@hotmail.com>
2022-01-30 02:20:50 -0300

    css and variables to english

--- index.js
+++ index.js
@@ -21,25 +21,25 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     const body = req.body
-    procurarPalavras(req, res, body)
+    searchWords(req, res, body)
 })
 
-const procurarPalavras = (req, res, body) => {
+const searchWords = (req, res, body) => {
     const { match, guess, wrong } = body;
     readFile('palavras.txt', (err, data) => {
         if (err) throw err;
-        let palavras = data.toString('utf-8').toLocaleLowerCase().split(' ');
+        let words = data.toString('utf-8').toLocaleLowerCase().split(' ');
     
-        const palavrasFiltradas = palavras.filter(palavra => {
+        const filteredWords = words.filter(word => {
             const from = "ãàáäâèéëêìíïîòóöôùúüûñ";
             const to   = "aaaaaeeeeiiiioooouuuun";
             for (var i=0, l=from.length ; i<l ; i++) {
-                palavra = palavra.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+                word = word.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
             }
     
             for(let i=0; i<5; i++) {
                 if (match[i]) {
-                    if(!(palavra.split('')[i] === (match[i]))) {
+                    if(!(word.split('')[i] === (match[i]))) {
                         return false;
                     }
                 }
@@ -48,21 +48,21 @@ const procurarPalavras = (req, res, body) => {
             const guesses = Object.values(guess).filter(k => k !== '');
 
             for(let i=0; i<guesses.length; i++){
-                if(!(palavra.indexOf(guesses[i]) > -1)){
+                if(!(word.indexOf(guesses[i]) > -1)){
                     return false;
                 }
             }
 
             for(let i=0; i<wrong.length; i++){
-                if(palavra.indexOf(wrong[i]) > -1){
+                if(word.indexOf(wrong[i]) > -1){
                     return false;
                 }
             }
 
-            return palavra;
+            return word;
             })
     
-            res.send(palavrasFiltradas);
+            res.send(filteredWords);
     });
 }
 
